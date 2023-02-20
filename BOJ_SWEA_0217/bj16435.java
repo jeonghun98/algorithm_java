80ec02c235d29400c8df8a77c91e60d7d661268f
JeongHun <52409864+jeonghun98@users.noreply.github.com>
2023-02-21 00:22:56 +0900

    Fix : [0217] BJ 16435_스네이크버드
     Fix : [0217] BJ 16435_스네이크버드

--- BOJ_SWEA_0217/bj16435.java
+++ BOJ_SWEA_0217/bj16435.java
@@ -5,7 +5,7 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Main_16435_김정훈 {
+public class bj16435 {
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
