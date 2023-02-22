eb93ffd0acf40170c40128843bcbd74f09f8c642
JeongHun <52409864+jeonghun98@users.noreply.github.com>
2023-02-22 21:36:13 +0900

    Fix : [0221] BJ 2638 치즈
    Fix : [0221] BJ 2638 치즈

--- BOJ_SWEA_0221/bj2638.java
+++ BOJ_SWEA_0221/bj2638.java
@@ -1,4 +1,4 @@
-package algorithm_java.BOJ_SWEA_2021;
+package algorithm_java.BOJ_SWEA_0221;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -18,14 +18,14 @@ public class bj2638 {
     public static class position {
         int x, y, time;
 
-        public position(int x, int y, int time) {
+        public position(int x, int y, int time) { // 위치, 시간
             this.x = x;
             this.y = y;
             this.time = time;
         }
     }
 
-    public static void air(int x, int y) {
+    public static void air(int x, int y) { // 외부공기 초기화 dfs 메소드
         if(data[x][y] == 1) return;
         data[x][y] = air_index;
 
@@ -38,29 +38,29 @@ public class bj2638 {
         }
     }
 
-    public static void cheese() {
+    public static void cheese() { //치즈 녹이기
 
         int time = 0;
-        while(!q.isEmpty()) {
+        while(!q.isEmpty()) { // 큐에 치즈 조각이 있다면 반복
             position p = q.poll();
-            if(time != p.time) {
+            if(time != p.time) {    // 저장된 시간보다 시간이 증가했다면 외부공기도 초기화
                 air_index++; air(0,0);
                 time = p.time;
             }
 
             int side = 0;
-            for(int i = 0; i < 4; i++) {
+            for(int i = 0; i < 4; i++) { // 사방탐색
                 int nx = p.x + dx[i];
                 int ny = p.y + dy[i];
                 if(nx < 0 || nx >= n || ny < 0 || ny >= m || data[nx][ny] == 0 || data[nx][ny] == 1)
                     continue;
-                side++;
+                side++; //외부와 접촉 count
             }
 
-            if(side < 2)
+            if(side < 2)    // 외부 접촉이 2변 이하라면 다시 큐에 넣기
                 q.offer(new position(p.x,p.y,p.time+1));
             else
-                data[p.x][p.y] = 0;
+                data[p.x][p.y] = 0; // 접촉이 2변 이상이라면 0으로 변경(치즈 녹이기)
 
             result = time+1;
         }
@@ -80,13 +80,13 @@ public class bj2638 {
             for(int j = 0; j < m; j++) {
                 data[i][j] = Integer.parseInt(st.nextToken());
                 if(data[i][j] == 1)
-                    q.offer(new position(i,j,0));
+                    q.offer(new position(i,j,0)); //치즈있는 위치 -> 큐에 넣기
             }
         }
 
-        air_index = 2;
-        air(0,0);
-        cheese();
+        air_index = 2; //외부공기
+        air(0,0); // 외부공기 값 변경
+        cheese();   // 치즈 녹이기
         System.out.println(result);
     }
 }
