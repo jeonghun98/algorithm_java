1c980d2a2a773e3400e9f00fecbc4dd0b687ac97
김정훈 <wjdgns7040@gmail.com>
2023-03-07 17:33:44 +0900

    Update: [0307] BJ 1541 잃어버린 괄호

---
+++ BOJ_SWEA_0307/bj1541.java
@@ -0,0 +1,34 @@
+package algorithm_java.BOJ_SWEA_0307;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class bj1541 {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String line = br.readLine();
+		String minus[] = line.split("-"); // - 로 분리하기
+		
+		int result = 0;
+		boolean first = true;
+		
+		for(String m : minus) {
+			String plus[] = m.split("\\+"); // + 로 분리하기
+			
+			int plus_tmp = 0;
+			
+			for(String p : plus)	// + 분리한 수 다 더하기
+				plus_tmp += Integer.parseInt(p);
+			
+			if(first) { // 처음 숫자라면 +
+				first = false;
+				result += plus_tmp;
+			}
+			else result -= plus_tmp; // 처음 숫자가 아니라면 다 -
+		}
+		System.out.println(result);
+	}
+}
