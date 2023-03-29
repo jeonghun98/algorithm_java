d5c313e849db6b54b70df7312674ab92a1a3c40c
JeongHun <wjdgns7040@gmail.com>
2023-03-29 21:56:00 +0900

    Fix: [0329] BJ 10971 외판원 순회 2
    Fix: [0329] BJ 10971 외판원 순회 2

--- BOJ_SWEA_0329/bj10971.java
+++ BOJ_SWEA_0329/bj10971.java
@@ -13,19 +13,18 @@ public class bj10971 {
 	static int result;
 	
 	public static void TSP(int start, int now, int cnt, int weight) {
-		if (weight > result) return;
-		
+		if (weight > result) return; // 비용이 현재 최종 min보다 크다면 return
+
+		// n개를 모두 방문했고 현재위치에서 다시 출발했던 도시로 올 수 있다면
 		if(cnt == n-1 && data[now][start] != 0) {
-			result = Math.min(result, weight + data[now][start]);
+			result = Math.min(result, weight + data[now][start]); // 최종 값 갱신
 			return;
 		}
 		
-		for(int i = 0; i < n; i++) {
-			if(visited[i]) continue;
+		for(int i = 0; i < n; i++) { // 순열
+			if(visited[i] || data[now][i] == 0) continue; // 방문한 경로 or 갈 수 없는 경로라면 continue
 			
-			if(data[now][i] == 0) continue;
-			
-			visited[i] = true;
+			visited[i] = true; // 방문체크 -> 재귀 -> 방문해제
 			TSP(start, i, cnt+1, weight + data[now][i]);
 			visited[i] = false;
 			
@@ -46,7 +45,8 @@ public class bj10971 {
 				data[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-		
+
+		// start 0 ~ n-1
 		for(int i = 0; i < n; i++) {
 			visited[i] = true;
 			TSP(i,i,0,0);
