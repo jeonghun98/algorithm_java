5093f442c6a10e13594c01b26ad61cd241bc1395
JeongHun <52409864+jeonghun98@users.noreply.github.com>
2024-02-08 12:41:07 +0900

    FIX : [0208] BOJ 14867 물통

--- BFS/bj14867.java
+++ BFS/bj14867.java
@@ -122,3 +122,5 @@ public class bj14867 {
         System.out.println(bfs());
     }
 }
+// 참고
+// https://velog.io/@sonypark/Java-equals-hascode-%EB%A9%94%EC%84%9C%EB%93%9C%EB%8A%94-%EC%96%B8%EC%A0%9C-%EC%9E%AC%EC%A0%95%EC%9D%98%ED%95%B4%EC%95%BC-%ED%95%A0%EA%B9%8C
