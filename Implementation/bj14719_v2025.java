06a7d46acbdf13762c6defa3cc0168bb1b246e47
jeonghun98 <wjdgns7040@gmail.com>
2025-07-28 23:56:10 +0900

    Update: [0728] BJ 14719 빗물

--- Implementation/bj14719_v2025.java
+++ Implementation/bj14719_v2025.java
@@ -3,56 +3,48 @@ package algorithm_java.Implementation;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-
 import java.util.StringTokenizer;
+
 // Baekjoon Online Judge 14719 빗물
 public class bj14719_v2025 {
-    static int h,w;
-    static int[] arr;
-    static int water;
-    public static void getRain(int maxH) {
-        int minus = maxH - arr[0];
-
-        int tmp = arr[0];
+    public static int getWater(int h, int w, int maxH, int water, int[] arr) {
+        int maxH2 = arr[0];
         for(int i = 0; i < w; i++) {
-            if(tmp < arr[i]) {
-                minus = maxH - arr[i];
-                tmp = arr[i];
+            if(maxH2 < arr[i]) {
+                maxH2 = arr[i];
             }
-            water -= minus;
+            water -= (maxH - maxH2);
+            if(maxH2 == maxH) break;
         }
 
-        minus = maxH - arr[w-1];
-        tmp = arr[w-1];
+        maxH2 = arr[w-1];
         for(int i = w-1; i >= 0; i--) {
-            if(tmp < arr[i]) {
-                minus = maxH - arr[i];
-                tmp = arr[i];
+            if(maxH2 < arr[i]) {
+                maxH2 = arr[i];
             }
-            water -= minus;
+            water -= (maxH - maxH2);
+            if(maxH2 == maxH) break;
         }
+        return water;
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        h = Integer.parseInt(st.nextToken());
-        w = Integer.parseInt(st.nextToken());
-        arr = new int[w];
+        int h = Integer.parseInt(st.nextToken());
+        int w = Integer.parseInt(st.nextToken());
 
-        int maxH = 0;
+        int[] arr = new int[w];
+        int maxH = -1, block = 0;
 
         st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < w; i++) {
+        for (int i = 0; i < w; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
-            water += (h-arr[i]);
-            maxH = Math.max(arr[i], maxH);
+            maxH = Math.max(maxH, arr[i]);
+            block += arr[i];
         }
-
-        water -= (h-maxH) * w;
-        getRain(maxH);
-        System.out.println(water);
+        System.out.println(getWater(h, w, maxH, h*w - block - ((h-maxH) * w), arr));
     }
 
 }
